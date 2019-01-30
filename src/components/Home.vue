6385af4a0e5b9c56750fec4a2add65ec25a3d070
Ong Xin Ying <ongxinying@Ongs-MacBook-Pro.local>
2019-01-30 23:28:37 +0800

    margin for homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="background1">
     <div id="overlay1" class="row justify-content-center">
-    <div class="col-sm-5" style="margin-top:120px">
+    <div class="col-sm-5" id="quoteBox">
   “A human being is a part of the whole called by us universe, a part limited in time and space. He experiences himself, his thoughts and feeling as something separated from the rest, a kind of optical delusion of his consciousness. This delusion is a kind of prison for us, restricting us to our personal desires and to affection for a few persons nearest to us. Our task must be to free ourselves from this prison by widening our circle of compassion to embrace all living creatures and the whole of nature in its beauty.” 
 ― Albert Einstein
     </div>
@@ -39,5 +39,8 @@ export default {
     color: #3f4d63;
     font-weight: 400;
     font-size: 18px;
+}
+#quoteBox {
+    margin: 120px 20px 0px 20px;
 }
 </style>
